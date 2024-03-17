9518a2ebed2e531eafdcc66da854ffc0e4098f00
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-03-17 19:16:00 +0300

    last fix of exception, tests are green, task is done

--- task1/src/main/java/by/verabei/Application.java
+++ task1/src/main/java/by/verabei/Application.java
@@ -9,9 +9,10 @@ public class Application {
     public static void main(String[] args) {
         Folder rootFolder = new Folder("root");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter path to add\n" +
-                "Or enter print to print out tree structure\n" +
-                "Or enter quit to stop the program");
+        System.out.println("""
+                Enter path to add
+                Or enter print to print out tree structure
+                Or enter quit to stop the program""");
 
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
--- task1/src/main/java/by/verabei/builder/FileSystemBuilder.java
+++ task1/src/main/java/by/verabei/builder/FileSystemBuilder.java
@@ -9,8 +9,8 @@ import java.util.*;
 public class FileSystemBuilder {
     public static FileSystemComponent buildFileSystem(String input, Folder root) {
         List<String> components = new ArrayList<>(List.of(input.split("/")));
-        Folder changingFolder = new Folder(root);
-        Folder currentFolder = changingFolder;
+        Folder saveRootFolder = new Folder(root);
+        Folder currentFolder = root;
 
         for (int i = 1; i < components.size(); i++) {
             String componentName = components.get(i);
@@ -21,10 +21,11 @@ public class FileSystemBuilder {
              if (currentFolder.getContent().stream()
                     .noneMatch(component1 -> componentName.equals(component1.getName()))) {
                 if (componentName.contains(".")) {
-                    currentFolder.addComponent(new File(componentName));
                     if (i != components.size() - 1) {
+                        root = saveRootFolder;
                         throw new IllegalArgumentException();
                     }
+                    currentFolder.addComponent(new File(componentName));
                 } else {
                     Folder folder = new Folder(componentName);
                     currentFolder.addComponent(folder);
@@ -41,8 +42,6 @@ public class FileSystemBuilder {
                 }
             }
         }
-
-        root = changingFolder;
         return root;
     }
 }
