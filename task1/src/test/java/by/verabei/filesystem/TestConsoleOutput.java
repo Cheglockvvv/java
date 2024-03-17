c5520099e1a1dddabb62fbda66119c25d9a04902
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-03-17 15:49:51 +0300

    Add tree console output test

--- task1/src/test/java/by/verabei/filesystem/TestConsoleOutput.java
+++ task1/src/test/java/by/verabei/filesystem/TestConsoleOutput.java
@@ -1,4 +1,32 @@
 package by.verabei.filesystem;
 
+import by.verabei.builder.FileSystemBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestConsoleOutput {
+    @Test
+    public void testConsoleOutput() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        String input = "root/folder1/folder2/file.txt";
+        FileSystemComponent root = FileSystemBuilder.buildFileSystem(input);
+
+        try {
+            System.setOut(new PrintStream(outputStream));
+            root.display();
+
+            String consoleOutput = outputStream.toString().trim();
+            String expectedOutput = "root/\n\tfolder1/\n\tfolder2/\n\tfile.txt";
+
+            assertEquals(consoleOutput, expectedOutput);
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
 }
