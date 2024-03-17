0d69d363d7dfc80f3282764623d652cf97fa44a9
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-03-17 16:18:53 +0300

    finished tree structure builder

--- task1/src/main/java/by/verabei/builder/FileSystemBuilder.java
+++ task1/src/main/java/by/verabei/builder/FileSystemBuilder.java
@@ -1,17 +1,32 @@
 package by.verabei.builder;
 
 import by.verabei.filesystem.File;
+import by.verabei.filesystem.FileSystemComponent;
 import by.verabei.filesystem.Folder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class FileSystemBuilder {
-    private Folder root;
+    public static FileSystemComponent buildFileSystem(String input, Folder root) {
+        List<String> components = new ArrayList<>(List.of(input.split("/")));
+        Folder currentFolder = root;
 
-    public FileSystemBuilder(String rootName) {
-        this.root = new Folder(rootName);
-    }
+        for (String component : components) {
+            if (!component.isEmpty()
+                    && currentFolder.getContent().stream()
+                    .anyMatch(component1 -> Objects.equals(component1.getName(), component))) {
+                if (component.contains(".")) {
+                    currentFolder.addComponent(new File(component));
+                } else {
+                    Folder folder = new Folder(component);
+                    currentFolder.addComponent(folder);
+                    currentFolder = folder;
+                }
+            }
+        }
 
-    public FileSystemBuilder addFile(String fileName) {
-        File file = new File(fileName);
-        return new FileSystemBuilder("root");
+        return root;
     }
 }
