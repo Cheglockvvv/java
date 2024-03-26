edfd193bac288f3c79bafbff4059264f135734bd
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-03-26 22:06:18 +0300

    set up properties

--- task2/src/main/java/com/vorobey/util/ConnectionUtil.java
+++ task2/src/main/java/com/vorobey/util/ConnectionUtil.java
@@ -1,10 +1,25 @@
 package com.vorobey.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+
     private ConnectionUtil() {}
     public static Connection get() {
-
+         try {
+             return DriverManager.getConnection(
+                     PropertiesUtil.get(URL_KEY),
+                     PropertiesUtil.get(USERNAME_KEY),
+                     PropertiesUtil.get(PASSWORD_KEY)
+             );
+         } catch (SQLException e) {
+             throw new RuntimeException(e);
+         }
     }
 }
