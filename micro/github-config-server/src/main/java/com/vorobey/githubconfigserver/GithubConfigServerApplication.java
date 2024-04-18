862ba1b040e7d48c6b4f284c828640cb802e155c
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-04-18 20:15:23 +0300

    added spring cloud config server and tested several instances of one microservice (load balancer)

---
+++ micro/github-config-server/src/main/java/com/vorobey/githubconfigserver/GithubConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.vorobey.githubconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubConfigServerApplication.class, args);
+	}
+
+}
