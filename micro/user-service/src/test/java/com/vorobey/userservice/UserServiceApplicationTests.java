c973e2fb783cd16151673d459a3770a9c9b2afba
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-04-22 16:35:32 +0300

    rename and add all the services

---
+++ micro/user-service/src/test/java/com/vorobey/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vorobey.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
