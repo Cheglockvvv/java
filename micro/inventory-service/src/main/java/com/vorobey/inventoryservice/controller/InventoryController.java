20b8b6ee14833ca54d59711ac011d34c1126f709
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-05-31 14:46:37 +0300

    InventoryController.java init

---
+++ micro/inventory-service/src/main/java/com/vorobey/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,9 @@
+package com.vorobey.inventoryservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+}
