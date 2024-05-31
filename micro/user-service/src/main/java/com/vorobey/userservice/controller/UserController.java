6cf414ac95e351a88983b57d9c0361c064b4bf6a
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-05-31 11:55:57 +0300

    added getAllProductWithStock() method to user controller on /user/products endpoint

--- micro/user-service/src/main/java/com/vorobey/userservice/controller/UserController.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/controller/UserController.java
@@ -1,15 +1,28 @@
 package com.vorobey.userservice.controller;
 
+import com.vorobey.userservice.service.UserService;
+import com.vorobey.userservice.util.ProductWithStock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/test")
     public String instruments() {
         return "instruments";
     }
 
+    @GetMapping("/products")
+    public List<ProductWithStock> getAllProductWithStock() {
+        return userService.getAllProductsWithStock();
+    }
 }
