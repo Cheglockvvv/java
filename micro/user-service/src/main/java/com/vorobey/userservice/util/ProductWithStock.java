e9513805327ea3be34d560964437a4733a203330
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-05-31 11:51:52 +0300

    Added allArgsConstructor to ProductWithStock class and finished getAllProductsWithStock() method. Now I can implement user controller

--- micro/user-service/src/main/java/com/vorobey/userservice/util/ProductWithStock.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/util/ProductWithStock.java
@@ -1,9 +1,11 @@
 package com.vorobey.userservice.util;
 
 import com.vorobey.productservice.entity.ProductEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductWithStock {
     private ProductEntity product;
     private int stock;
