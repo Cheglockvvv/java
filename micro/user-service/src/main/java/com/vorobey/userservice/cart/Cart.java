6edaf2aac6fd4db96e0271441fcb77c71f0b7da8
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-16 17:26:47 +0300

    Added @Data annotation to both classes

--- micro/user-service/src/main/java/com/vorobey/userservice/cart/Cart.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/cart/Cart.java
@@ -1,10 +1,12 @@
 package com.vorobey.userservice.cart;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 @Entity
 public class Cart {
     @Id
