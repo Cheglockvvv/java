73f6dae585487d5ea8906cf69a04921e45d1a5fb
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-16 10:45:10 +0300

    empty method for adding items in a cart

--- micro/user-service/src/main/java/com/vorobey/userservice/controller/UserController.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/controller/UserController.java
@@ -3,9 +3,7 @@ package com.vorobey.userservice.controller;
 import com.vorobey.userservice.service.UserService;
 import com.vorobey.userservice.util.ProductWithStock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +19,13 @@ public class UserController {
         return "instruments";
     }
 
-    @GetMapping("/products")
-    public List<ProductWithStock> getAllProductWithStock() {
-        return userService.getAllProductsWithStock();
+    @PostMapping("/add")
+    public String addToCart(@RequestBody ProductWithStock productWithStock) {
+        return "empty";
     }
+
+//    @GetMapping("/products")
+//    public List<ProductWithStock> getAllProductWithStock() {
+//        return userService.getAllProductsWithStock();
+//    }
 }
