c09ba4e09247b675403581d854eefdaf9d817819
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-16 11:33:56 +0300

    added a new method to an interface

--- micro/user-service/src/main/java/com/vorobey/userservice/service/UserService.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/service/UserService.java
@@ -1,7 +1,10 @@
 package com.vorobey.userservice.service;
 
+import com.vorobey.userservice.cart.Cart;
+
 import java.util.List;
 
 public interface UserService {
+    Cart addToCart(Long userId, Long productId, Integer quantity);
     //List<ProductWithStock> getAllProductsWithStock();
 }
