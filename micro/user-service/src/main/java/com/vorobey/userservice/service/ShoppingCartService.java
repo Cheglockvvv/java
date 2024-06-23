aa323fde191057de8ae144117fc20a394f157a29
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-23 15:12:38 +0300

    Add CRUD operations to cart service

--- micro/user-service/src/main/java/com/vorobey/userservice/service/ShoppingCartService.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/service/ShoppingCartService.java
@@ -1,6 +1,11 @@
 package com.vorobey.userservice.service;
 
-public interface ShoppingCartService {
-    void addToCart();
+import com.vorobey.userservice.cart.Cart;
+import com.vorobey.userservice.cart.CartItem;
 
+public interface ShoppingCartService {
+    void addToCart(Long userId, CartItem cartItem);
+    Cart getCart(Long userId);
+    void removeFromCart(Long userId, CartItem cartItem);
+    void clearCart(Long userId);
 }
