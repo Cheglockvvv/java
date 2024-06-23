b939eab3662141e7e96ff7c51de87b6c68b20677
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-23 17:15:15 +0300

    clear userController

--- micro/user-service/src/main/java/com/vorobey/userservice/controller/UserController.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/controller/UserController.java
@@ -2,7 +2,9 @@ package com.vorobey.userservice.controller;
 
 import com.vorobey.userservice.cart.Cart;
 import com.vorobey.userservice.cart.CartItem;
+import com.vorobey.userservice.service.ShoppingCartService;
 import com.vorobey.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +14,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class UserController {
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/test")
     public String instruments() {
         return "instruments";
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<String> addToCart(@RequestParam Long userId, @RequestBody CartItem cartItem) {
-        return new ResponseEntity<>("hello", HttpStatus.OK);
-    }
 }
