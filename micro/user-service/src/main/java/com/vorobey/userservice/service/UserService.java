163cc76dfd4fe45b646a1eb7ad2040f94cde775c
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-23 15:36:27 +0300

    remove all the methods of UserServiceImpl.java

--- micro/user-service/src/main/java/com/vorobey/userservice/service/UserService.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/service/UserService.java
@@ -1,10 +1,5 @@
 package com.vorobey.userservice.service;
 
-import com.vorobey.userservice.cart.Cart;
-
-import java.util.List;
-
 public interface UserService {
-    //Cart addToCart(Long userId, Long productId, Integer quantity);
 
 }
--- micro/user-service/src/main/java/com/vorobey/userservice/service/impl/UserServiceImpl.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/service/impl/UserServiceImpl.java
@@ -6,30 +6,4 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
-//    @Autowired
-//    private CartRepository cartRepository;
-//
-//    @Override
-//    public Cart addToCart(Long userId, Long productId, Integer quantity) {
-//        Optional<Cart> optionalCart = cartRepository.findByUserId(userId);
-//        Cart cart = optionalCart.orElseGet(Cart::new);
-//        cart.setUserId(userId);
-//
-//        Optional<CartItem> optionalCartItem = cart.getItems().stream()
-//                .filter(item -> item.getProductId().equals(productId))
-//                .findFirst();
-//
-//        if (optionalCartItem.isPresent()) {
-//            CartItem cartItem = optionalCartItem.get();
-//            cartItem.setQuantity(cartItem.getQuantity() + quantity);
-//        } else {
-//            CartItem cartItem = new CartItem();
-//            cartItem.setProductId(productId);
-//            cartItem.setQuantity(quantity);
-//            cart.getItems().add(cartItem);
-//        }
-//
-//        return cartRepository.save(cart);
-//    }
-
 }
