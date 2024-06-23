4d05ed89910abf5a04dfed4a86c7350d660e516e
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-23 17:28:07 +0300

    removeFromCart, getCart and clearCart methods

--- micro/user-service/src/main/java/com/vorobey/userservice/controller/ShoppingCartController.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/controller/ShoppingCartController.java
@@ -1,10 +1,14 @@
 package com.vorobey.userservice.controller;
 
+import com.vorobey.userservice.cart.Cart;
 import com.vorobey.userservice.cart.CartItem;
 import com.vorobey.userservice.service.ShoppingCartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cart")
@@ -15,5 +19,21 @@ public class ShoppingCartController {
     @PostMapping("/add")
     public void addToCart(@RequestParam Long userId, @RequestBody CartItem cartItem) {
         shoppingCartService.addToCart(userId, cartItem);
+    }
+
+    @GetMapping
+    public ResponseEntity<Cart> getCart(@RequestParam Long userId) {
+        Optional<Cart> cartOptional = shoppingCartService.getCart(userId);
+        return cartOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/remove")
+    public void removeFromCart(@RequestParam Long userId, @RequestParam Long productId) {
+        shoppingCartService.removeFromCart(userId, productId);
+    }
+
+    @PostMapping("/clear")
+    public void clearCart(@RequestParam Long userId) {
+        shoppingCartService.clearCart(userId);
     }
 }
