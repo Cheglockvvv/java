aa5d8f9cd01ae6e2dce7c867b0dbdcf77cb37518
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-24 15:20:54 +0300

    add @EnableKafka annotation

--- micro/user-service/src/main/java/com/vorobey/userservice/UserServiceApplication.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/UserServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
+@EnableKafka
 @SpringBootApplication
 public class UserServiceApplication {
 
