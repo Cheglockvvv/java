411c91dc22467b7d318226a259e947deba6e7cfa
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-06-24 15:25:58 +0300

    refactor producer properties

--- micro/user-service/src/main/java/com/vorobey/userservice/kafka/producer/KafkaProducerConfig.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/kafka/producer/KafkaProducerConfig.java
@@ -18,11 +18,7 @@ public class KafkaProducerConfig {
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        return new DefaultKafkaProducerFactory<>(producerProps());
     }
 
     @Bean
@@ -30,5 +26,13 @@ public class KafkaProducerConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
+    private Map<String, Object> producerProps() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG, 10);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return configProps;
+    }
 
 }
