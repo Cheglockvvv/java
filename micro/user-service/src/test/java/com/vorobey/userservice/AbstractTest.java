840d909486306f560f73f3eb96e3cba3af806ecf
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-07-01 22:48:24 +0300

    add abstractTest class

---
+++ micro/user-service/src/test/java/com/vorobey/userservice/AbstractTest.java
@@ -0,0 +1,8 @@
+package com.vorobey.userservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public abstract class AbstractTest {
+}
