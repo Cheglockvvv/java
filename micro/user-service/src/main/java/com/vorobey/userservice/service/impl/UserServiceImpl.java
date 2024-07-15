6f782ffa73e2bffe86e6afd82f7540888430dcbe
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-07-15 13:10:32 +0300

    temporary null implementation

--- micro/user-service/src/main/java/com/vorobey/userservice/service/impl/UserServiceImpl.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/service/impl/UserServiceImpl.java
@@ -1,9 +1,40 @@
 package com.vorobey.userservice.service.impl;
 
+import com.vorobey.userservice.entity.user.User;
+import com.vorobey.userservice.repository.UserRepository;
 import com.vorobey.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+
+    @Override
+    public User registerUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User findUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+
+    }
 }
