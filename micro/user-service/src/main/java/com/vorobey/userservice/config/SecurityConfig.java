6bb37d7c10c726a7ac2bb5607066e493bc5f1dd7
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-07-15 13:11:46 +0300

    add security configuration

---
+++ micro/user-service/src/main/java/com/vorobey/userservice/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.vorobey.userservice.config;
+
+import jakarta.ws.rs.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain makeSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(customizer -> customizer.jwtAuthenticationConverter(jwtConverter())));
+        http.oauth2Login(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/user", HttpMethod.OPTIONS))
+                .permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/user"))
+                .hasRole("USER")
+                .requestMatchers(new AntPathRequestMatcher("/"))
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtConverter() {
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setPrincipalClaimName("preferred_username");
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+
+            Collection<GrantedAuthority> authorities = grantedAuthoritiesConverter.convert(jwt);
+
+            Map<String, Object> claims = jwt.getClaimAsMap("realm_access");
+            List<String> roles = (List<String>) claims.get("roles");
+
+            return Stream.concat(authorities.stream(),
+                            roles.stream()
+                                    .filter(role -> role.startsWith("ROLE"))
+                                    .map(SimpleGrantedAuthority::new))
+                    .toList();
+        });
+
+        return converter;
+    }
+
+}
