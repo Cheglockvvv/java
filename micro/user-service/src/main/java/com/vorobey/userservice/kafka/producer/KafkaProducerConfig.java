0428228d11abf4bb5930cef0581c60fa92393fbe
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-07-15 13:13:32 +0300

    temp disable kafka

--- micro/user-service/src/main/java/com/vorobey/userservice/kafka/producer/KafkaProducerConfig.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/kafka/producer/KafkaProducerConfig.java
@@ -15,7 +15,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+//@Configuration
 public class KafkaProducerConfig {
 
     @Bean
