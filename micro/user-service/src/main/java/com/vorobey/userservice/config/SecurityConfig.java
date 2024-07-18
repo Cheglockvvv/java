485550bff1cc7f6f88c300c2c1fbd12daca06692
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-07-18 13:09:26 +0300

    add EnableMethodSecurity annotation

--- micro/user-service/src/main/java/com/vorobey/userservice/config/SecurityConfig.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -23,6 +24,7 @@ import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
