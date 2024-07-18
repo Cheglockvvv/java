8536591e844a715fcb7aed0b6b6883c37f6430a0
Cheglock.vvv <cheglock.vvv@gmail.com>
2024-07-18 15:06:39 +0300

    add checkAndMakeOrder method and restTemplate dependencies

--- micro/user-service/src/main/java/com/vorobey/userservice/service/ShoppingCartService.java
+++ micro/user-service/src/main/java/com/vorobey/userservice/service/ShoppingCartService.java
@@ -8,4 +8,5 @@ public interface ShoppingCartService {
     Cart getCart(Long userId);
     void removeFromCart(Long userId, Long productId);
     void clearCart(Long userId);
+    String checkAndMakeOrder(Long userId);
 }
